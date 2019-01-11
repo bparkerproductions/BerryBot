06b04a1c78786991ddbfab0301ea0f06536de54d
Scott Reimels <bparkerproductions@gmail.com>
2019-01-10 22:08:40 -0900

    write recent question to file

--- modules/api/reddit.js
+++ modules/api/reddit.js
@@ -2,6 +2,7 @@ const auth = require("./../../auth.json");
 const Helpers = require('../helpers/helpers.js');
 const snoowrap = require("snoowrap");
 const reddits = require("./../../data/redditsMap.json");
+const fs = require("fs");
 
 //init reddit instance
 const reddit = new snoowrap({
@@ -26,7 +27,7 @@ module.exports = {
     else if(command == "idea" || command == "i") {
       this.mapTypeOfReddit(arguments, message, ideas);
     }
-    else if(command == "ideaHelp" || "ih") {
+    else if(command == "ideaHelp" || command == "ih") {
       this.questionHelp(arguments, message, ideas, "idea");
     }
   },
@@ -47,13 +48,26 @@ module.exports = {
     return !question.title.toLowerCase().includes("reddit");
   },
 
+  storeQuestion(question) {   
+      //set it in the file
+      reddits.recentQuestion.question = question;
+
+      //write it to the file
+      fs.writeFile('data/redditsMap.json', JSON.stringify(reddits, null, 2), (err) => {
+        if(err) return console.log(err);
+      });
+  },
+
   askQuestion(arguments, recieved, subreddit) {
     reddit.getSubreddit(subreddit)
     .getHot({'limit': 100})
-    .then( posts => {
+    .then( (posts, resolve) => {
       let questionIndex = Helpers.generateRandom(100);
       let question = posts.toJSON()[questionIndex]; //choose a question by index
-      recieved.channel.send(question.title);
+
+      recieved.channel.send(question.title).then(question => {
+        this.storeQuestion(question.content); //write it to file
+      })
     });
   },
 
